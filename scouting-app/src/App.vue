34a9beb22d6851ba8f2eddd1f7e40ab4d228f77a
Providence <123270106+bellonaDev@users.noreply.github.com>
2023-01-21 15:43:42 -0600

    fix packages idk

--- scouting-app/src/App.vue
+++ scouting-app/src/App.vue
@@ -58,14 +58,82 @@
 </template>
 
 <script>
-
+import firebase from "./firebaseInit";
+const db = firebase.firestore();
 export default {
-  name: 'app',
-  data(){
+  name: "app",
+  data() {
     return {
-      name: '',
-      employeesData: []
-    }
-  }
-}
+      name: "",
+      date: new Date().toISOString().slice(0, 10),
+      employeesData: [],
+      search: "",
+    };
+  },
+  methods: {
+    createEmployee(name, date) {
+      if (name != "") {
+        db.collection("employees")
+          .add({ date: date, name: name })
+          .then(() => {
+            console.log("Document successfully written!");
+            this.readEmployees();
+          })
+          .catch((error) => {
+            console.error("Error writing document: ", error);
+          });
+        this.name = "";
+      }
+    },
+    readEmployees() {
+      this.employeesData = [];
+      db.collection("employees")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.employeesData.push({
+              id: doc.id,
+              name: doc.data().name,
+              date: doc.data().date,
+            });
+            console.log(doc.id, " => ", doc.data());
+          });
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
+        });
+    },
+    updateEmployee(id, name, date) {
+      db.collection("employees")
+        .doc(id)
+        .update({
+          name: name,
+          date: date,
+        })
+        .then(() => {
+          console.log("Document successfully updated!");
+          this.readEmployees();
+        })
+        .catch((error) => {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        });
+    },
+    deleteEmployee(id) {
+      db.collection("employees")
+        .doc(id)
+        .delete()
+        .then(() => {
+          console.log("Document successfully deleted!");
+          this.readEmployees();
+        })
+        .catch((error) => {
+          console.error("Error removing document: ", error);
+        });
+    },
+  },
+  mounted() {
+    this.readEmployees();
+  },
+};
 </script>
