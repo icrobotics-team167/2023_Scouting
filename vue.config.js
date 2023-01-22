8b3fe5ede6e70a330a3cfd40f5870a9444aca884
Madison Kriege <46975132+madison-kriege@users.noreply.github.com>
2023-01-21 22:48:13 -0600

    Another stackOverflow answer to why this doesn't work

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/2023_Scouting/'
+  ? 'docs/'
   : '',
 })
