8cf31ef4fbc3e24c44b80dceb508d745c9979236
Providence <riverbpa04@gmail.com>
2023-02-15 18:42:36 -0600

    css

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,32 @@ import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+<template><div class="text-primary-lighten-1">text color</div></template>
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: '#0b5c06',
+    surface: '#000000',
+    primary: '#000000',
+    'primary-darken-1': '#000000',
+    secondary: '#000000',
+    'secondary-darken-1': '#000000',
+    error: '#000000',
+    info: '#000000',
+    success: '#000000',
+    warning: '#000000',
+    text: '#000000'
+  }
+}
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'myCustomLightTheme',
+    themes:{
+      myCustomLightTheme,
+    }
+  }
+}
 )
